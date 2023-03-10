0e436a05bdcd6dfdf88903a12c7b68b459f956c0
Mike O'Brien <mikeob9@gmail.com>
2023-03-10 13:46:31 -0500

    Add daily occurrence scraper

--- daily_occurrence_scraper.R
+++ daily_occurrence_scraper.R
@@ -2,6 +2,7 @@ library(rvest)
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(googlesheets4)
 
 whales_html <- read_html('http://dcs.whoi.edu/mdoc0722/mdoc0722_mdoc.shtml') 
 
@@ -32,16 +33,48 @@ whales <- whales |>
   )
 
 
-whales <- whales |> 
-  mutate(month = month(Date),
+whale_summary <- whales |> 
+  mutate(month = month(Date, label = T),
          year = year(Date)) |> 
   group_by(year, month) |> 
-  summarize(monitoring_days = n(),
-            n_sw_pd = sum(`Sei whale` == 'possibly detected'),
-            n_sw_d =  sum(`Sei whale` == 'detected'),
-            n_fw_pd = sum(`Fin whale` == 'possibly detected'),
-            n_fw_d =  sum(`Fin whale` == 'detected'),
-            n_rw_pd = sum(`Right whale` == 'possibly detected'),
-            n_rw_d =  sum(`Right whale` == 'detected'),
-            n_hw_pd = sum(`Humpback whale` == 'possibly detected'),
-            n_hw_d =  sum(`Humpback whale` == 'detected'))
+  summarize(`Monitoring Days` = n(),
+            `# Days PD only SW` = sum(`Sei whale` == 'possibly detected'),
+            `# Days D SW` =  sum(`Sei whale` == 'detected'),
+            `# Days PD only FW` = sum(`Fin whale` == 'possibly detected'),
+            `# Days D FW` =  sum(`Fin whale` == 'detected'),
+            `# Days PD only RW` = sum(`Right whale` == 'possibly detected'),
+            `# Days D RW` =  sum(`Right whale` == 'detected'),
+            `# Days PD only HW` = sum(`Humpback whale` == 'possibly detected'),
+            `# Days D HW` =  sum(`Humpback whale` == 'detected'))
+
+# Add summary row
+whale_summary <- whale_summary |> 
+  bind_rows(
+    whale_summary |> 
+      ungroup () |>
+      summarize(across(`Monitoring Days`:`# Days D HW`, sum))
+  )
+
+gs4_auth(path = Sys.getenv('GDRIVE_PAT'))
+
+# remove previously-scraped sheet
+occurrence_sheet <- 'https://docs.google.com/spreadsheets/d/1hGFPbmarhzRI_zf-E01mVEZKCjP7dezYsTQihKhUgZs/edit#gid=0'
+sheet_delete(
+  occurrence_sheet,
+  grep('Scraper - ',
+       sheet_names(occurrence_sheet)
+  )
+)
+
+# Add newly-scraped sheet
+sheet_write(
+  whales,
+  occurrence_sheet,
+  sheet = paste0('Scraper - Full table ', Sys.time())
+)
+
+sheet_write(
+  whale_summary,
+  occurrence_sheet,
+  sheet = paste0('Scraper - Summary ', Sys.time())
+)
